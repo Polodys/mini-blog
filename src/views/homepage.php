7bd8567eac528ccba1519d70c0ee7dd2d7a11be2
Polodys <vincent.carof@gmail.com>
2024-08-08 18:09:34 +0200

    Layout

--- src/views/homepage.php
+++ src/views/homepage.php
@@ -1,18 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Accueil - Liste des articles</h1>
-    <ul>
-        <?php foreach ($posts as $post): ?>
-            <li>
+<?php $title = "Mon mini blog - Accueil"; ?>
+
+<?php ob_start(); ?>
+
+<h1>Liste des articles</h1>
+<hr>
+
+<p>Cliquez sur un titre pour afficher l'article en entier</p>
+<hr>
+
+<ul>
+    <?php foreach ($posts as $post): ?>
+        <li>
             <a href=""><?= htmlspecialchars($post->getTitle()) ?></a>
-            </li>
-        <?php endforeach ?>
-    </ul>
-</body>
-</html>
+        </li>
+    <?php endforeach ?>
+</ul>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require 'layout.php' ?>
---
+++ src/views/partials/header.php
@@ -0,0 +1,13 @@
+<header>
+    <nav>
+        <a href="index.php">Mon mini blog</a>
+        <ul>
+            <li>
+                <a href="index.php">Accueil</a>
+            </li>
+            <li>
+                <a href="index.php?action=create-post-form">Nouvel article</a>
+            </li>
+        </ul>
+    </nav>
+</header>
